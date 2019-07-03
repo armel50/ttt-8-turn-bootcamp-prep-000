708a82e0517ed1539ca79ede384df5de2a73b726
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-03 00:37:13 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -46,8 +46,8 @@ end
 
 def turn(board)
   puts "please enter 1-9:"
-  #input = gets.strip
-  #index =input_to_index(input)
+  input = gets.strip
+  index =input_to_index(input)
  #checkValid= valid_move?(board, index)
   # move(board, index)
   while valid_move?(board,index) == false
